3baab5494cb415a980b954067bcd174b3c9b2aaf
Solange5 <sollybright5@yahoo.com>
2019-06-20 21:04:53 -0400

    weekb wed code challenge

---
+++ WeekB/StringReverse.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class StringReverse {
+
+	private static String in;
+
+	public static void main(String[] args) {
+		
+		//System.out.println("Final word should be (.)");
+		String str = "my name is solange";
+		
+		//split after space
+		String a[] = str.split(" ");
+		
+		for (int i=0; i<a.length;i++) {
+			
+			
+			System.out.print(a[i]+ " ");
+			
+		}
+		
+		System.out.println("");
+		
+		for(int i=a.length-1;i>=0;i--) {
+			
+			System.out.print(a[i] + " ");
+			
+		}}}
+	
+		
+		//recursion is a method that calls it self it becomes easier and 
+		//and reaches a point where it no longer calls it self. 
+		
+		
+	//	private static void stackWords() {
+	//Scanner Scanner = new Scanner(System.in); 
+			//accepting only one word at a time, user puts only one word
+		/*	String word = in.nextLine();
+			 //if the word is equals to period, this is the only time, it needs to stop.
+			//but if user don't enter stack, just keep calling stack word
+			if(word.equals(".")) 
+				System.out.println();
+			System.out.println("Enter the names one per line");
+		
+		//	else  
+				stackWords();
+				System.out.println(word);
+			}
+			
+			*/
+	
+		
+			
+		
+	
+
+		
+	
+		
+	
+	
+	
+
+
